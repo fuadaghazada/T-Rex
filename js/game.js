785c6adf1e9421fdcb8f0911180fc14f5823ed52
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-04-30 13:17:45 +0300

    just a test

--- js/game.js
+++ js/game.js
@@ -284,7 +284,7 @@ function getCookie(cname)
 
 window.onload
 {
-    var dates = [new Date("9/10/2018"), new Date("9/9/2018"), new Date("8/1/2018"), new Date("09/20/2018")];
+    var dates = [new Date("9/10/2019"), new Date("9/9/2019"), new Date("8/1/2019"), new Date("09/20/2019"), new Date("4/30/2019")];
 
     function check_special_date()
     {
